2f331cc4975e5f8383b51775d5c68150894ff8d2
Jacob Howard <jacob@mutagen.io>
2022-01-28 11:52:04 +0200

    fix: relocate and update exec transport after rebase

--- pkg/synchronization/protocols/exec/protocol.go
+++ pkg/synchronization/protocols/exec/protocol.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net"
+	"io"
 
 	"github.com/mutagen-io/mutagen/pkg/agent"
-	"github.com/mutagen-io/mutagen/pkg/agent/transports/exec"
+	"github.com/mutagen-io/mutagen/pkg/agent/transport/exec"
 	"github.com/mutagen-io/mutagen/pkg/logging"
 	"github.com/mutagen-io/mutagen/pkg/synchronization"
 	"github.com/mutagen-io/mutagen/pkg/synchronization/endpoint/remote"
@@ -15,18 +15,19 @@ import (
 )
 
 // protocolHandler implements the synchronization.ProtocolHandler interface for
-// connecting to remote endpoints by running a shell command.
+// connecting to remote endpoints by running a shell command. It uses the agent
+// infrastructure over a custom command transport.
 type protocolHandler struct{}
 
 // dialResult provides asynchronous agent dialing results.
 type dialResult struct {
-	// connection is the connection returned by agent dialing.
-	connection net.Conn
+	// stream is the stream returned by agent dialing.
+	stream io.ReadWriteCloser
 	// error is the error returned by agent dialing.
 	error error
 }
 
-// Connect connects to an SSH endpoint.
+// Connect connects to an exec endpoint.
 func (h *protocolHandler) Connect(
 	ctx context.Context,
 	logger *logging.Logger,
@@ -52,7 +53,7 @@ func (h *protocolHandler) Connect(
 		return nil, errors.New("exec URL contains internal parameters")
 	}
 
-	// Create an SSH agent transport.
+	// Create an exec-based agent transport.
 	transport, err := exec.NewTransport(url.Host, prompter)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create exec transport: %w", err)
@@ -65,35 +66,35 @@ func (h *protocolHandler) Connect(
 	// cancellation.
 	go func() {
 		// Perform the dialing operation.
-		connection, err := agent.Dial(logger, transport, agent.ModeSynchronizer, prompter)
+		stream, err := agent.Dial(logger, transport, agent.ModeSynchronizer, prompter)
 
-		// Transmit the result or, if cancelled, close the connection.
+		// Transmit the result or, if cancelled, close the stream.
 		select {
-		case results <- dialResult{connection, err}:
+		case results <- dialResult{stream, err}:
 		case <-ctx.Done():
-			if connection != nil {
-				connection.Close()
+			if stream != nil {
+				stream.Close()
 			}
 		}
 	}()
 
 	// Wait for dialing results or cancellation.
-	var connection net.Conn
+	var stream io.ReadWriteCloser
 	select {
 	case result := <-results:
 		if result.error != nil {
 			return nil, fmt.Errorf("unable to dial agent endpoint: %w", result.error)
 		}
-		connection = result.connection
+		stream = result.stream
 	case <-ctx.Done():
 		return nil, errors.New("connect operation cancelled")
 	}
 
 	// Create the endpoint client.
-	return remote.NewEndpoint(connection, url.Path, session, version, configuration, alpha)
+	return remote.NewEndpoint(stream, url.Path, session, version, configuration, alpha)
 }
 
 func init() {
-	// Register the TCP protocol handler with the synchronization package.
+	// Register the exec protocol handler with the synchronization package.
 	synchronization.ProtocolHandlers[urlpkg.Protocol_Exec] = &protocolHandler{}
 }
